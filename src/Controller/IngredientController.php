c9096873e634db36587bcecd3098584f5815f156
Julien <julien.dousse@gmail.com>
2023-03-08 10:10:20 +0100

    Update form + twig  add ingredient

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,5 +32,32 @@ class IngredientController extends AbstractController
             10 /*limit per page*/
         );
         return $this->render('pages/ingredient/index.html.twig', ['ingredients' => $ingredients]);
+    }
+
+    #[Route('/ingredient/nouveau', name: 'ingredient_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+
+            $manager->persist($ingredient);
+            $manager->flush();
+
+
+            $this->addFlash(
+                'success',
+                'Votre ingrédient a été créé avec succès !'
+            );
+            return $this->redirectToRoute('app_ingredient');
+        }
+
+        return $this->render('pages/ingredient/new.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }

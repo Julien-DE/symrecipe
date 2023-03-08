2f748f3680c3534f79aa4089ce9b39e61e7bac50
Julien <julien.dousse@gmail.com>
2023-03-08 09:10:02 +0100

    ingredients/index twig update

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -11,7 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IngredientController extends AbstractController
 {
-    #[Route('/ingredient', name: 'app_ingredient')]
+    /**
+     * This function display all ingredients
+     *
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+
+    #[Route('/ingredient', name: 'app_ingredient', methods: ['GET'])]
     public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
         $ingredients = $paginator->paginate(

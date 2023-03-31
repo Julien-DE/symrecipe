b676a22601e5dd09c4336b508c997778468663c8
Julien <julien.dousse@gmail.com>
2023-03-31 11:44:42 +0200

    TRy some tests

---
+++ tests/Functional/HomePageTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomePageTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Bievenue sur SymRecipe');
+    }
+}
